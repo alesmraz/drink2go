274cfb0d4159f4bee42c5a27e418bfeea26cc25e
Ales Mraz <am@antstudio.cz>
2020-02-27 22:23:00 +0100

    restyle

--- assets/js/Filter.js
+++ assets/js/Filter.js
@@ -1,41 +1,35 @@
 export default class Filter {
   constructor() {
     this.filterElement = document.querySelector('form#filter')
+    this.control = document.querySelector('form#filter .filterControl')
+    this.controlChild = document.querySelector('form#filter .filterControlChild')
+    this.groupAll = 'all'
   }
 
   init() {
-    if (this.filterElement) {
-      this.collapses = this.getCollapses();
-      this.controlCollapses()
+    if (this.filterElement && this.control && this.controlChild) {
+      const controlBtns = [...this.control.querySelectorAll('.filter__button')]
+      if (controlBtns) {
+        controlBtns.forEach((btn) => {
+          btn.addEventListener('click', () => {
+            controlBtns.forEach((controlBtn) => controlBtn.classList.remove('filter__button--active'))
+            btn.classList.add('filter__button--active')
 
-      window.addEventListener('resize', () => {
-        this.controlCollapses()
-      });
+            const btnGroup = btn.dataset.group
 
-    }
-  }
+            if (btnGroup === this.groupAll) {
+              const allBtn = [...this.controlChild.querySelectorAll(`.filter__button`)]
+              if (allBtn) allBtn.forEach((ibtn) => ibtn.classList.add('filter__button--show'))
+            } else {
+              const btnToShow = [...this.controlChild.querySelectorAll(`.filter__button[data-group="${btnGroup}"]`)]
+              const btnToHide = [...this.controlChild.querySelectorAll(`.filter__button:not([data-group="${btnGroup}"])`)]
 
-  getCollapses() {
-    return [...this.filterElement.querySelectorAll('.collapse')]
-  }
-
-  controlCollapses() {
-    if (this.collapses.length) {
-
-      // sm
-      if (this.getScreenSize() < 768) {
-        this.collapses.forEach(collapse => {
-          collapse.classList.remove('show')
+              if (btnToShow) btnToShow.forEach((ibtn) => ibtn.classList.add('filter__button--show'))
+              if (btnToHide) btnToHide.forEach((ibtn) => ibtn.classList.remove('filter__button--show'))
+            }
+          })
         })
       }
-
-
     }
-
   }
-
-  getScreenSize() {
-    return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
-  }
-
 }
